fb21c9f38da7e140f5fbc85645a63043115a6e6b
daberry_wh <daberry@williamhill.us>
2022-01-19 08:29:39 -0800

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ docker push daylanberry/multi-worker:latest
 docker push daylanberry/multi-client:$SHA
 docker push daylanberry/multi-server:$SHA
 docker push daylanberry/multi-worker:$SHA
-kubectl apply - k8s
+kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=daylanberry/multi-server:$SHA
 kubectl set image deployments/client-deployment client=daylanberry/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=daylanberry/multi-worker:$SHA
